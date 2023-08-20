1137d439bfc15105e54008298578c21947363439
daveanue <66977330+daveanue@users.noreply.github.com>
2023-08-19 20:23:51 -0400

    created api file & utils to getProduct

---
+++ api/apiLinks.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = '';
+export const PRODUCT_URL = `${BASE_URL}/products`;
+export const USER_API = '';
---
+++ src/utils/getProducts.tsx
@@ -0,0 +1,8 @@
+import { Product } from '../../interfaces/productTypes';
+import { PRODUCT_URL } from '../../api/apiLinks';
+
+export async function getProducts(): Promise<Product[]> {
+  const response = await fetch(PRODUCT_URL);
+  const data = await response.json();
+  return data as Product[];
+}
