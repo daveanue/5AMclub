f652cb586f83445274ffefe08fb08297338c29ca
daveanue <66977330+daveanue@users.noreply.github.com>
2023-08-30 13:01:27 -0400

    added typescript interface for votingItemProps

--- interfaces/productTypes.ts
+++ interfaces/productTypes.ts
@@ -1,10 +1,16 @@
 export interface Product {
   id : number;
   name: string;
+  voteCount: number;
   description?: string;
   price?: number;
 }
 
+
+
 export interface VotingItemProps {
+  key: number,
+  id: number,
   product: Product;
+  onVoteClick: (voteProductId: number) => void
 }
