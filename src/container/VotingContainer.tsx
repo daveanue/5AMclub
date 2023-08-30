267c92a76fa8a37e369a34b9e1bf45b8426eb0a1
daveanue <66977330+daveanue@users.noreply.github.com>
2023-08-30 13:05:33 -0400

    refactored container to pass down handleVote function down to individual voting Items. allowing put request to serverto update voteCount and pulling newest voteCount to sync frontend with server

--- src/container/VotingContainer.tsx
+++ src/container/VotingContainer.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect} from 'react';
 import { Product } from '../../interfaces/productTypes';
 import { getProducts } from '../utils/getProducts';
+import { sendVote } from '../utils/sendVote';
 import VotingItem  from '../components/VotingItem';
 
 // shift + option + f auto format codes
@@ -8,6 +9,23 @@ import VotingItem  from '../components/VotingItem';
 export default function VotingContainer() {
   const [voteProducts, setvoteProducts] = useState<Product[]>([]);
 
+  const handleVote = async (voteProductId: number, setvoteProducts: React.Dispatch<React.SetStateAction<Product[]>>) => {
+    try {
+      const updatedVoteCount = await sendVote(voteProductId);
+
+      const updatedVoteProducts = voteProducts.map(product => {
+        if (product.id === voteProductId) {
+          return {...product, voteCount: updatedVoteCount };
+        }
+        // if id doesn't match, return product as is
+        return product;
+      });
+      setvoteProducts(updatedVoteProducts);
+    }
+    catch (error) {
+      console.error('Error updating vote Count', error);
+    }
+  }
   useEffect(() => {
     async function fetchData() {
       try {
@@ -19,10 +37,18 @@ export default function VotingContainer() {
     }
     fetchData();
   }, []);
+
+
+
   return (
       <div>
-        {voteProducts.map((product) => (
-          <VotingItem key={product.id} product={product}/>
+      {voteProducts.map((product : Product) => (
+          <VotingItem
+          key={product.id}
+          id={product.id}
+          product={product}
+          onVoteClick={()=> handleVote(product.id, setvoteProducts)}
+          />
         ))}
       </div>
   );
