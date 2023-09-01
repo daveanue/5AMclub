8ff63c51ca852ff16964eb790b6b8978533d2556
daveanue <66977330+daveanue@users.noreply.github.com>
2023-09-01 13:49:37 -0400

    moved addToCart from app level to product container level

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Product } from '../interfaces/Product';
 import VotingContainer from './container/VotingContainer'
 import { CartProvider } from './context/CartContext';
+import { ProductProvider } from './context/ProductContext';
 import ProductContainer from './container/ProductContainer';
 function App() {
   /*
@@ -11,21 +12,7 @@ function App() {
   to server side.
   */
 
-  const addToCart = (product: Product) => {
-    // if the product already exist in the cart, then we need to increment the quantity by 1
-    setCartItems((prevCartItems) => {
-      const existingCartItems = prevCartItems.find(item => item.product.id === product.id);
-
-      if (existingCartItems) {
-        return prevCartItems.map(item =>
-          item.product.id === product.id ? {...item, quantity: item.quantity + 1}
-          : item);
-      } else {
-        // product yet to exist in cart, add product with quantity of 1
-        return [...prevCartItems, {product, quantity: 1}];
-      }
-    })
-  }
+  
 
   const removeItemFromCart = (productId: number) => {
     setCartItems((prevCartItems) => {
@@ -45,10 +32,12 @@ function App() {
   return (
     <div>
       <CartProvider>
+      <ProductProvider>
         <ProductContainer />
+      </ProductProvider>
       </CartProvider>
-      <VotingContainer />
 
+      <VotingContainer />
     </div>
   )
 }
