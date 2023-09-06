732c336db3477717a1c022cf616a1ffd01dce731
daveanue <66977330+daveanue@users.noreply.github.com>
2023-09-06 12:07:46 -0400

    update text to add to cart

--- src/components/ProductComponent.tsx
+++ src/components/ProductComponent.tsx
@@ -1,14 +1,15 @@
 import { ProductComponentProps } from "../../interfaces/Product";
 
-export default function ProductComponent({ product, addToCart} : ProductComponentProps) {
-
-
+export default function ProductComponent({
+  product,
+  addToCart
+} : ProductComponentProps) {
   return (
     <div>
       <h3> {product.name} </h3>
       <h4> {product.price} </h4>
       <p> {product.description} </p>
-      <button onClick={()=>addToCart(product)}>+</button>
+      <button onClick={()=>addToCart(product)}>Add to Cart</button>
     </div>
   )
 }
