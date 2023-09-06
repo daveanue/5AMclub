8751b3f927e465a7b828eb020a8f4519ef1fda51
daveanue <66977330+daveanue@users.noreply.github.com>
2023-09-05 23:06:31 -0400

    added input field form to allow user to adjust quantity of item for purchase with ease

--- src/components/CartComponent.tsx
+++ src/components/CartComponent.tsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import { CartComponentProps } from '../../interfaces/CartComponent'
 
-export default function CartComponent({ cartItem } : CartComponentProps) {
+export default function CartComponent({ cartItem, removeItem, editItemQuantity } : CartComponentProps) {
+  const [quantityInput, setQuantityInput] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const newValue = event.target.value;
+    setQuantityInput(newValue); // Keep the input as a string
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (quantityInput !== '') {
+      const parsedValue = parseInt(quantityInput, 10);
+      if (!isNaN(parsedValue)) {
+        await editItemQuantity(cartItem.product.id, parsedValue);
+        console.log('submitted value', parsedValue);
+        setQuantityInput(''); // Clear the input after successful submission
+      }
+    }
+  }
+
   return (
     <div>
       <h3> {cartItem.product.name} </h3>
       <h4> {cartItem.product.price} </h4>
       <p> {cartItem.quantity} </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={quantityInput}
+          onChange={handleInputChange}
+        />
+        <button type="submit">+</button>
+      </form>
+      <button onClick={() => removeItem(cartItem.product.id)}>Remove</button>
     </div>
   )
 }
